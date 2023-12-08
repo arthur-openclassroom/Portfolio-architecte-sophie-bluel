e988b3b70020f184253ec42878b72bd031bad879
Arthur Lambotte <arthur1209@orange.fr>
2023-12-08 15:33:54 +0100

    Step 1-2 : Data filter

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,11 +1,9 @@
 // Récupération des travaux depuis le serveur
-alert("ok");
-const http_response = await fetch('http://localhost:5678/api/works');
-const works = await http_response.json();
+const http_response_works = await fetch('http://localhost:5678/api/works');
+const works = await http_response_works.json();
 
-function generateGallery(works){
-    for (let i = 0; i < works.length; i++) {
-        const work = works[i];
+function generateDynamicGallery(works) {
+    for (const work of works) {
         const divGallery = document.querySelector(".gallery");
         const figureElement = document.createElement("figure");
         // Création des balises 
@@ -13,12 +11,52 @@ function generateGallery(works){
         imageElement.src = work.imageUrl;
         const titleElement = document.createElement("figcaption");
         titleElement.innerText = work.title;
-        
-        // On rattache la balise article a la section Fiches
+
+        // On rattache la balise figure au div gallery
         divGallery.appendChild(figureElement);
         figureElement.appendChild(imageElement);
-        figureElement.appendChild(titleElement);    
-     }
+        figureElement.appendChild(titleElement);
+    }
 }
 
-generateGallery(works);
+const http_response_categories = await fetch('http://localhost:5678/api/categories');
+const categories = await http_response_categories.json();
+
+function cleanActiveButton() {
+    const divPortfolio = document.querySelector(".filters");
+    for (let button of divPortfolio.children) {
+        button.removeAttribute("active", "");    
+    }
+}
+
+function generateDynamicFilters(categories) {
+    for (const categorie of categories) {
+        const divPortfolio = document.querySelector(".filters");
+        const btnElement = document.createElement("button");
+        btnElement.setAttribute("class", "btn-filter")
+        btnElement.innerText = categorie.name;
+        // Ajout du filtre au bouton
+        btnElement.addEventListener("click", function () {
+            const worksFiltered = works.filter(function (work) {
+                return work.category.id == categorie.id;
+            });
+            document.querySelector(".gallery").innerHTML = "";
+            generateDynamicGallery(worksFiltered);
+            cleanActiveButton();
+            btnElement.setAttribute("active", "");
+        });
+        // On rattache la balise bouton au div
+        divPortfolio.appendChild(btnElement);
+    }
+}
+
+const btnElement = document.querySelector("#btn-filter-all");
+    btnElement.addEventListener("click", function () {
+    document.querySelector(".gallery").innerHTML = "";
+    generateDynamicGallery(works);
+    cleanActiveButton();
+    btnElement.setAttribute("active", "");
+});
+
+generateDynamicGallery(works);
+generateDynamicFilters(categories);
